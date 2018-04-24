51a020bec6ebdb44118edcc91d2759e574a43b89
lanzongjun <939972634@qq.com>
2018-04-24 13:55:01 +0800

    add helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,1 +1,17 @@
 <?php
+
+if (!function_exists('is_valid_phone')) {
+    function is_valid_phone($number)
+    {
+        if (!is_string($number)) {
+            return false;
+        }
+        $number = strtr($number, array('-' => '', ' ' => '', '(' => '', ')' => ''));
+        $pattern = '/^(?:\+?0?86)?(?:17951)?(1\d{10})$/';
+        if (preg_match($pattern, $number, $matches)) {dd($matches);
+            return $matches[1];
+        }
+
+        return false;
+    }
+}
